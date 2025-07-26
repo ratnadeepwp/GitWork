78ec98216f16a26f88860a5d751e632ae7497765
ratna <ratnadeep42@gmail.com>
2025-07-26 22:08:12 +0530

    final commit

---
+++ src/com/java/Arch.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Arch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Final build from kol arch team");
+
+	}
+
+}
