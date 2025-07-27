096e48df1db41cdc465a84062c88465754ac7199
ratna <ratnadeep42@gmail.com>
2025-07-27 16:12:08 +0530

    Pracetice  work.

---
+++ src/com/git/practice/demoWork.java
@@ -0,0 +1,12 @@
+package com.git.practice;
+
+public class demoWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Pracetice Work");
+
+	}
+
+}
